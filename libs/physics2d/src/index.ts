8125efd4ffdaf286ca63eb50299386b3a1e0c2ea
pavlik228 <pleonov@greencode.pro>
2025-08-28 00:23:46 +0800

    chore(release): bump @lagless/ packages (patch)

--- libs/physics2d/src/index.ts
+++ libs/physics2d/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/misc/index.js';
-export * from './lib/physics2d-world.js';
 export * from './lib/physics2d-config.js';
 export * from './lib/physics2d-runner.js';
 export * from './lib/physics2d-simulation.js';
